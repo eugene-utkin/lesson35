cc7a43451d49b88cbf6b2ce1910d28aa86fe1938
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-03 14:18:39 +0500

    Cancel Button Added

--- public/app.js
+++ public/app.js
@@ -72,3 +72,11 @@ function cart_get_orders()
 		return orders;
 }
 
+function cancel_order()
+{
+	window.localStorage.clear();
+	update_orders_input();
+	update_orders_button();
+	return false;
+}
+
